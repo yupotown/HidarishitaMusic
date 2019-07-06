3cf2e5f5e991ccfa4de4195d7cc5f3f73bc0dd64
Yupo <yupotown@gmail.com>
2019-07-07 05:08:29 +0900

    UI を調整

--- HidarishitaMusic/Main.cpp
+++ HidarishitaMusic/Main.cpp
@@ -8,20 +8,25 @@ private:
 
 	Sound m_sound;
 
+	const Font font;
+	String title;
+	int titleOffset;
+
 public:
 
 	SoundPlayer()
-		: m_gui(GUIStyle::Default)
+		: m_gui(GUIStyle::Default),
+		font(20, Typeface::Medium, FontStyle::Outline)
 	{
-		m_gui.add(L"PlayButton", GUIButton::Create(L"Play"));
+		m_gui.add(L"PlayButton", GUIButton::Create(L">"));
 
-		m_gui.add(L"PauseButton", GUIButton::Create(L"Pause"));
+		m_gui.add(L"PauseButton", GUIButton::Create(L"||"));
 
 		m_gui.addln(L"OpenButton", GUIButton::Create(L"Open"));
 
-		m_gui.add(L"Slider", GUISlider::Create(0.0, 1.0, 0.0, 500));
+		m_gui.add(L"Slider", GUISlider::Create(0.0, 1.0, 0.0, Window::Width() - 100));
 
-		m_gui.setPos((Window::Width() - m_gui.getRect().w) / 2, 360);
+		m_gui.setPos((Window::Width() - m_gui.getRect().w) / 2, Window::Height() - 100);
 	}
 
 	void update()
@@ -43,6 +48,9 @@ public:
 			m_sound.pause();
 
 			m_sound = Dialog::OpenSound();
+
+			title = L"曲タイトル";
+			titleOffset = 0;
 		}
 
 		if (m_gui.slider(L"Slider").hasChanged)
@@ -51,10 +59,23 @@ public:
 		}
 
 		m_gui.slider(L"Slider").setValue(m_sound.streamPosSec() / m_sound.lengthSec());
+
+		titleOffset -= 1;
+
+		const auto region = font(title).region();
+		if (titleOffset < -region.w) {
+			titleOffset += region.w + Window::Width() / 2;
+		}
 	}
 
 	void drawVisualizer() const
 	{
+		const auto region = font(title).region();
+		font(title).draw(22 + titleOffset, 0);
+		font(title).draw(22 + titleOffset + region.w + Window::Width() / 2, 0);
+		Rect(0, 0, 22, 40).draw(Color(0, 255, 0));
+		font(L"♪").draw(2, 0);
+
 		if (!m_sound.isPlaying())
 		{
 			return;
@@ -62,18 +83,21 @@ public:
 
 		const auto fft = FFT::Analyze(m_sound);
 
-		for (int32 i = 0; i < 640; ++i)
+		for (int32 i = 0; i < Window::Width(); ++i)
 		{
-			const double size = Pow(fft.buffer[i], 0.6) * 500;
+			const double size = Pow(fft.buffer[i], 0.6) * Window::Height();
 
-			RectF(i, 200 + size, 1, -size * 2).draw(HSV(240 - i));
+			RectF(i, Window::Height() / 2 + size, 1, -size * 2).draw(Color(0, 0, 255));
 		}
 	}
 };
 
 void Main()
 {
-	Graphics::SetBackground(Color(70, 80, 170));
+	Window::Resize(320, 240);
+	Window::SetTitle(L"HidarishitaMusic");
+
+	Graphics::SetBackground(Color(0, 255, 0));
 
 	SoundPlayer soundPlayer;
 
