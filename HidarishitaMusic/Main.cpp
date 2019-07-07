da8041c5f89d98a5857a9364a93d5b083531cf62
Yupo <yupotown@gmail.com>
2019-07-07 11:42:01 +0900

    音量調整機能を追加

--- HidarishitaMusic/Main.cpp
+++ HidarishitaMusic/Main.cpp
@@ -1,5 +1,11 @@
 ﻿# include <Siv3D.hpp>
 
+#define BUTTON_PLAY L"ButtonPlay"
+#define BUTTON_PAUSE L"ButtonPause"
+#define BUTTON_OPEN L"ButtonOpen"
+#define SLIDER_POSITION L"SliderPosition"
+#define SLIDER_VOLUME L"SliderVolume"
+
 class SoundPlayer
 {
 private:
@@ -18,32 +24,34 @@ public:
 		: m_gui(GUIStyle::Default),
 		font(20, Typeface::Medium, FontStyle::Outline)
 	{
-		m_gui.add(L"PlayButton", GUIButton::Create(L">"));
+		m_gui.add(BUTTON_PLAY, GUIButton::Create(L">"));
 
-		m_gui.add(L"PauseButton", GUIButton::Create(L"||"));
+		m_gui.add(BUTTON_PAUSE, GUIButton::Create(L"||"));
 
-		m_gui.addln(L"OpenButton", GUIButton::Create(L"Open"));
+		m_gui.addln(BUTTON_OPEN, GUIButton::Create(L"Open"));
 
-		m_gui.add(L"Slider", GUISlider::Create(0.0, 1.0, 0.0, Window::Width() - 100));
+		m_gui.addln(SLIDER_POSITION, GUISlider::Create(0.0, 1.0, 0.0, Window::Width() - 100));
 
-		m_gui.setPos((Window::Width() - m_gui.getRect().w) / 2, Window::Height() - 100);
+		m_gui.add(SLIDER_VOLUME, GUISlider::Create(0.0, 1.0, 1.0, Window::Width() - 100));
+
+		m_gui.setPos((Window::Width() - m_gui.getRect().w) / 2, 140);
 	}
 
 	void update()
 	{
-		m_gui.button(L"PlayButton").enabled = (m_sound && !m_sound.isPlaying());
+		m_gui.button(BUTTON_PLAY).enabled = (m_sound && !m_sound.isPlaying());
 
-		m_gui.button(L"PauseButton").enabled = m_sound.isPlaying();
+		m_gui.button(BUTTON_PAUSE).enabled = m_sound.isPlaying();
 
-		if (m_gui.button(L"PlayButton").pushed)
+		if (m_gui.button(BUTTON_PLAY).pushed)
 		{
 			m_sound.play();
 		}
-		else if (m_gui.button(L"PauseButton").pushed)
+		else if (m_gui.button(BUTTON_PAUSE).pushed)
 		{
 			m_sound.pause();
 		}
-		else if (m_gui.button(L"OpenButton").pushed)
+		else if (m_gui.button(BUTTON_OPEN).pushed)
 		{
 			m_sound.pause();
 
@@ -53,17 +61,23 @@ public:
 			titleOffset = 0;
 		}
 
-		if (m_gui.slider(L"Slider").hasChanged)
+		if (m_gui.slider(SLIDER_POSITION).hasChanged)
 		{
-			m_sound.setPosSec(m_sound.lengthSec() * m_gui.slider(L"Slider").value);
+			m_sound.setPosSec(m_sound.lengthSec() * m_gui.slider(SLIDER_POSITION).value);
 		}
 
-		m_gui.slider(L"Slider").setValue(m_sound.streamPosSec() / m_sound.lengthSec());
+		if (m_gui.slider(SLIDER_VOLUME).hasChanged)
+		{
+			m_sound.setVolume(m_gui.slider(SLIDER_VOLUME).value);
+		}
+
+		m_gui.slider(SLIDER_POSITION).setValue(m_sound.streamPosSec() / m_sound.lengthSec());
 
 		titleOffset -= 1;
 
 		const auto region = font(title).region();
-		if (titleOffset < -region.w) {
+		if (titleOffset < -region.w)
+		{
 			titleOffset += region.w + Window::Width() / 2;
 		}
 	}
@@ -85,16 +99,16 @@ public:
 
 		for (int32 i = 0; i < Window::Width(); ++i)
 		{
-			const double size = Pow(fft.buffer[i], 0.6) * Window::Height();
+			const double size = Pow(fft.buffer[i], 0.5) * (240 - 80);
 
-			RectF(i, Window::Height() / 2 + size, 1, -size * 2).draw(Color(0, 0, 255));
+			RectF(i, 240 / 2 - size, 1, size * 2).draw(Color(0, 0, 255));
 		}
 	}
 };
 
 void Main()
 {
-	Window::Resize(320, 240);
+	Window::Resize(320, 300);
 	Window::SetTitle(L"HidarishitaMusic");
 
 	Graphics::SetBackground(Color(0, 255, 0));
